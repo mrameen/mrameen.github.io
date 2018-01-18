a623875f7f56f97c90e81db40ef70af2a3564bb3
mrameen <coconline2015@gmail.com>
2018-01-19 03:03:09 +0800

    v1.0

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,10 @@
-
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyAt024MrvhPHqQ7UFPw2DvEk0HCpkDhFaA",
     authDomain: "pilgrims-database.firebaseapp.com",
     databaseURL: "https://pilgrims-database.firebaseio.com",
     projectId: "pilgrims-database",
-    storageBucket: "",
+    storageBucket: "pilgrims-database.appspot.com",
     messagingSenderId: "426442286486"
   };
   firebase.initializeApp(config);
